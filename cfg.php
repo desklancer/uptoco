b01a1b0147f1f5c0e81a9fe983b0ad34ba80da52
desklancer <44214742+desklancer@users.noreply.github.com>
2021-07-26 02:08:26 +0530

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,45 @@
+<?php
+//User-Agent
+
+$uag = "xxx";
+
+//Doge
+$dogecookie = "xxx";
+//Bcn
+$bcncookie = "xxx";
+//dgb
+$dgbcookie = "xxx";
+//trx
+$trxcookie = "xxx";
+//neo
+$neocookie = "xxx";
+//ppc
+$ppccookie = "xxx";
+//Waves
+$wavescookie = "xxx";
+//lsk
+$lskcookie = "xxx";
+//rdd
+$rddcookie = "xxx";
+//strax
+$straxcookie = "xxx";
+//exs
+$exscookie = "xxx";
+//bch
+$bchcookie = "xxx";
+//ada
+$adacookie = "xxx";
+//etc
+$etccookie = "xxx";
+//exg
+$exgcookie = "xxx";
+//zen
+$zencookie = "xxx";
+//kmd
+$kmdcookie = "xxx";
+//pivx
+$pivxcookie = "xxx";
+//xtz
+$xtzcookie = "xxx";
+//vtc
+$vtccookie = "xxx";
